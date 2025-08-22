2662dba7c4bad7ae0017989ae7e90e162c9da3f3
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2025-08-22 11:25:56 -0400

    sign out button

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,11 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { AuthContext } from "@/lib/auth";
+import { useContext } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const authState = useContext(AuthContext)
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-xl font-bold">
         Welcome to Nativewind!
       </Text>
+      <Button onPress={authState.signOut}><Text>Sign Out</Text></Button>
     </View>
   );
 }
