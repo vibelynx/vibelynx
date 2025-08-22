8e2a4c831499f1bdd62e4e979fc4d80e2315e95b
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2025-08-22 11:25:50 -0400

    better sign in ui

--- app/signin.tsx
+++ app/signin.tsx
@@ -7,9 +7,15 @@ export default function Index() {
   const authState = useContext(AuthContext)
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Sign In Page</Text>
-      <Button onPress={authState.signIn}><Text>Sign In</Text></Button>
+    <View className="h-screen mx-10">
+      <Text className="dark:text-white text-5xl text-center font-semibold mt-auto mb-20">Share music across
+        every platform
+      </Text>
+      <View className="w-full gap-5 mb-20">
+        <Button className="w-full" onPress={authState.signIn}><Text>Continue with Email</Text></Button>
+        <Button className="w-full" onPress={authState.signIn}><Text>Continue with Apple</Text></Button>
+        <Button className="w-full" onPress={authState.signIn}><Text>Continue with Spotify</Text></Button>
+      </View>
     </View>
   );
 }
