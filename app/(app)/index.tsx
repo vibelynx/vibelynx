0cc396fb5a953ebf41447bc6ee47bbb938f6376e
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2025-08-26 01:43:22 -0400

    debug: get access toek button

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,18 +1,35 @@
 import { Button } from "@/components/ui/button";
-import { AuthContext } from "@/lib/auth";
+import { Text } from "@/components/ui/text"
+import { authClient, AuthContext } from "@/lib/auth";
 import { useRouter } from "expo-router";
-import { useContext } from "react";
-import { Text, View } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import { View } from "react-native";
 
 export default function Index() {
+  const [accessToken, setAccessToken] = useState<string | undefined>(undefined)
   const authState = useContext(AuthContext)
   const router = useRouter()
+
+  const getAccessToken = async () => {
+    const { data } = await authClient.getAccessToken({
+      providerId: "spotify",
+    })
+
+    setAccessToken(data?.accessToken)
+  }
+
+  useEffect(() => {
+    getAccessToken()
+
+    console.log(accessToken)
+  }, [])
 
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-xl font-bold">
         Welcome to (app)/index!
       </Text>
+      <Button onPress={() => getAccessToken()}><Text>Get Access Token</Text></Button>
       <Button onPress={() => router.push(`/(app)/track?id=${"bc0cba17759d905b"}`)}><Text>Track Page</Text></Button>
       <Button onPress={authState.signOut}><Text>Sign Out</Text></Button>
     </View>
