a026334ac4b00e07a6ec6af3b9ab41c664c2e5aa
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2025-08-28 14:59:12 -0400

    init home screen

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Text } from "@/components/ui/text"
 import { authClient, AuthContext } from "@/lib/auth";
-import { useRouter } from "expo-router";
+import { Image } from "expo-image";
+import { Link, useRouter } from "expo-router";
 import { useContext, useEffect, useState } from "react";
-import { View } from "react-native";
+import { SafeAreaView, View, StyleSheet, ScrollView } from "react-native";
 
 export default function Index() {
   const [accessToken, setAccessToken] = useState<string | undefined>(undefined)
@@ -25,13 +27,56 @@ export default function Index() {
   }, [])
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold">
-        Welcome to (app)/index!
-      </Text>
-      <Button onPress={() => getAccessToken()}><Text>Get Access Token</Text></Button>
-      <Button onPress={() => router.push(`/(app)/track?id=${"bc0cba17759d905b"}`)}><Text>Track Page</Text></Button>
-      <Button onPress={authState.signOut}><Text>Sign Out</Text></Button>
-    </View>
+    <SafeAreaView>
+      <View className="h-screen mx-4 mt-2 gap-4">
+        <View className="flex-row items-center justify-between">
+          <View className="flex-row items-center gap-2">
+            <View className="w-10 h-10">
+              <Image style={styles.image} source="https://vibelynx.app/spotify.svg" />
+            </View>
+            <Text variant="large">Isaiah Hamilton</Text>
+          </View>
+          <View className="flex-row items-center gap-4">
+            <View className="w-10 h-10">
+              <Image style={styles.image} source="https://vibelynx.app/spotify.svg" />
+            </View>
+            <View className="w-10 h-10">
+              <Image style={styles.image} source="https://vibelynx.app/spotify.svg" />
+            </View>
+            <View className="w-10 h-10">
+              <Image style={styles.image} source="https://vibelynx.app/spotify.svg" />
+            </View>
+          </View>
+        </View>
+        <Input placeholder="Search" />
+        <View className="gap-2">
+          <Text variant="large">Recently Played</Text>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => (
+              <Link href="/(app)/track?id=bc0cba17759d905b">
+                <View className="w-20 h-20">
+                  <Image style={styles.artwork} source="https://i.scdn.co/image/ab67616d0000b273028e0d1464c08f971df5071b" />
+                </View>
+              </Link>
+            ))}
+          </ScrollView>
+        </View>
+      </View>
+    </SafeAreaView >
   );
 }
+
+const styles = StyleSheet.create({
+  artwork: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    borderRadius: 15
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    borderRadius: 0
+  },
+});
