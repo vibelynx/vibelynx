d0b1ac6617e01c17acf4b0271a3ff16101d4b360
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2025-09-18 14:05:43 -0400

    feat: album screen

---
+++ app/(app)/album.tsx
@@ -0,0 +1,80 @@
+import { Text, StyleSheet, View, SafeAreaView } from "react-native"
+import { Image } from 'expo-image'
+import { useLocalSearchParams, Link } from "expo-router";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { GetAlbum, Album } from "@vibelynx/vibelynx-js"
+
+export default function TrackScreen() {
+  const { id, url } = useLocalSearchParams<{ id?: string; url?: string }>();
+  const [album, setAlbum] = useState<any>(null)
+
+  useEffect(() => {
+    if (id) {
+      GetAlbum(id)
+        .then((data: { album: Album, error: Error | null }) => setAlbum(data.album))
+    }
+
+    if (url) {
+    }
+  }, [id])
+
+  return (
+    <SafeAreaView>
+      <View className="h-screen mx-10 mt-10 gap-10">
+        <View className="gap-5">
+          <View className="w-80 h-80 mx-auto">
+            <Image
+              style={styles.artwork}
+              source={album?.artwork}
+              contentFit="cover"
+            />
+          </View>
+          <View className="gap-2.5">
+            <Text className="dark:text-white text-2xl text-center font-semibold">
+              {album?.title}
+            </Text>
+            <View className="flex flex-row gap-2 w-fit mx-auto">
+              {album?.artists?.map((artist: any) => (
+                <Text key={artist.id} className="dark:text-white text-lg font-medium text-center">
+                  {artist.name}
+                </Text>
+              ))}
+            </View>
+            <Button onPress={() => { }}><Text>Play</Text></Button>
+          </View>
+        </View>
+        <View className="gap-5">
+          <Text className="dark:text-white text-lg font-medium">Open In</Text>
+          <View className="flex flex-row gap-5">
+            {album?.platforms?.map((platform: any) => (
+              <Link href={platform.url} key={platform.key} className="w-20 h-20">
+                <Image
+                  style={styles.image}
+                  source={`https://vibelynx.app/${platform.key}.svg`}
+                  contentFit="cover"
+                />
+              </Link>
+            ))}
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  artwork: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    borderRadius: 25
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    borderRadius: 0
+  },
+});
+
