d2a6a7cb61bfb5bc521f44a09f8be3b57e9fe211
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2025-11-02 15:44:39 -0500

    fresh start
    fresh start

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,5 @@
-import "../global.css"
-import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from "@react-navigation/native";
-import { Redirect, Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { NAV_THEME } from "@/lib/constants";
-import { useColorScheme } from "@/lib/useColorScheme";
-import { AuthProvider, useAuth } from "@/lib/auth";
-
-const LIGHT_THEME: Theme = {
-  ...DefaultTheme,
-  colors: NAV_THEME.light,
-};
-const DARK_THEME: Theme = {
-  ...DarkTheme,
-  colors: NAV_THEME.dark,
-};
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const { isDarkColorScheme } = useColorScheme();
-  const { session } = useAuth()
-
-  if (session) {
-    return <Redirect href="/(app)" />
-  }
-
-  return (
-    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-      <AuthProvider>
-        <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
-        <Stack screenOptions={{ headerShown: false }} />
-      </AuthProvider>
-    </ThemeProvider>
-  )
+  return <Stack />;
 }
